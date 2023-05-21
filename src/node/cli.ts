8bf6186585da97d418cf9fabf349948277b6ae04
LicoriceBear <Sioncovy@gmail.com>
2023-05-21 19:16:11 +0800

    🎉 init: 新建项目&配置项目cli

---
+++ src/node/cli.ts
@@ -0,0 +1,16 @@
+import cac from 'cac'
+import { createDevServer } from './dev'
+
+const cli = cac('ansion').version('0.0.1').help()
+
+cli.command('dev [root]', 'start dev server').action(async (root: string) => {
+  const server = await createDevServer(root)
+  await server.listen()
+  server.printUrls()
+})
+
+cli.command('build [root]', 'build in prod').action(async (root: string) => {
+  console.log('build', root)
+})
+
+cli.parse()
